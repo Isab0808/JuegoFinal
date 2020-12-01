7d235e24f021f32d010ae16ec6321086db459de1
Isab0808 <isab0808@gmail.com>
2020-12-01 17:31:43 -0500

    Segundo cambio, hasta Nivel 1 instrucciones

--- src/Principal.java
+++ src/Principal.java
@@ -20,26 +20,32 @@ public class Principal extends PApplet{
 	int xBotonInicio;
 	int yBotonInicio;
 	
+// x y y de Boton Instrucciones
+		int xBotonInstrucciones;
+		int yBotonInstrucciones;
+	
 	int [][] mapa;
 	
 	@Override
 	public void setup() {
-		pantalla = new PImage[15];
+		pantalla = new PImage[17];
 		pantalla[0] = loadImage("PantallaInicio.jpg");
 		pantalla[1] = loadImage("BotonInicio.png");
 		pantalla[2] = loadImage("BotonInicioOprimido.png");
 		pantalla[3] = loadImage("InstruccionesG.jpg");
 		pantalla[4] = loadImage("BotonSiguienteIG.png");
-		pantalla[5] = loadImage("Ins.Nivel1.jpg");
-		pantalla[6] = loadImage("BotonSiguienteN1.png");
-		pantalla[7] = loadImage("EscenarioN1.jpg");
-		pantalla[8] = loadImage("NuevaPartidaN1.png");
-		pantalla[9] = loadImage("VidaN1.png");
-		pantalla[10] = loadImage("Ene.FuegoN1.png");
-		pantalla[11] = loadImage("Herra.ExtintorN1.png");
-		pantalla[12] = loadImage("Herra.MangueraN1.png");
-		pantalla[13] = loadImage("Muro1.png");
-		pantalla[14] = loadImage("Muro2.png");
+		pantalla[5] = loadImage("BotonSiguienteIGOprimido.png");
+		pantalla[6] = loadImage("Ins.Nivel1.jpg");
+		pantalla[7] = loadImage("BotonSiguienteN1.png");
+		pantalla[8] = loadImage("BotonSiguienteOprimidoN1.png");
+		pantalla[9] = loadImage("EscenarioN1.jpg");
+		pantalla[10] = loadImage("NuevaPartidaN1.png");
+		pantalla[11] = loadImage("VidaN1.png");
+		pantalla[12] = loadImage("Ene.FuegoN1.png");
+		pantalla[13] = loadImage("Herra.ExtintorN1.png");
+		pantalla[14] = loadImage("Herra.MangueraN1.png");
+		pantalla[15] = loadImage("Muro1.png");
+		pantalla[16] = loadImage("Muro2.png");
 		
 		
 		estado = 0;
@@ -47,6 +53,10 @@ public class Principal extends PApplet{
 	// x y y de Boton Inicio
 		xBotonInicio = 480;
 		yBotonInicio = 430;
+		
+	// x y y de Boton Instrucciones
+		xBotonInstrucciones= 1060;
+		yBotonInstrucciones = 565;
 		
 		mapa = new int [10][10];
 		for (int i = 0; i < 10; i++) {
@@ -78,8 +88,92 @@ public class Principal extends PApplet{
 				image(pantalla[2], xBotonInicio, yBotonInicio);}
 			
 			break;
-
-		default:
+			
+		case 1:
+		//INSTRUCCIONES GENERALES
+			//Imagen Instrucciones Generales
+			imageMode(CENTER);
+			image(pantalla[3], 600, 350);
+			imageMode(CORNER);
+				
+			// BOTON INICIO
+			// Imagen Boton Instrucciones
+			image(pantalla[4], xBotonInstrucciones, yBotonInstrucciones);
+				
+			// Si esta el mouse encima del Boton Instrucciones mostrar imagen Boton Instrucciones Oprimido
+			if (mouseX > xBotonInstrucciones && mouseX < xBotonInstrucciones + 106 
+				&& mouseY > yBotonInstrucciones && mouseY < yBotonInstrucciones + 107) {
+				image(pantalla[5], xBotonInstrucciones, yBotonInstrucciones);}
+				
+				break;
+				
+		case 2:
+		//INSTRUCCIONES NIVEL 1
+			//Imagen Instrucciones Nivel 1
+			imageMode(CENTER);
+			image(pantalla[6], 600, 350);
+			imageMode(CORNER);
+					
+			// BOTON INICIO
+			// Imagen Boton Instrucciones Nivel 1
+			image(pantalla[7], xBotonInstrucciones, yBotonInstrucciones);
+					
+			// Si esta el mouse encima del Boton Instrucciones mostrar imagen Boton Instrucciones Oprimido
+			if (mouseX > xBotonInstrucciones && mouseX < xBotonInstrucciones + 106 
+				&& mouseY > yBotonInstrucciones && mouseY < yBotonInstrucciones + 107) {
+				image(pantalla[8], xBotonInstrucciones, yBotonInstrucciones);}
+					
+			break;
+		}
+	}
+	
+	@Override
+	public void mousePressed() {
+		switch (estado) {
+		case 0:
+		//INICIO
+			//Imagen Inicio
+			imageMode(CENTER);
+			image(pantalla[0], 600, 350);
+			imageMode(CORNER);
+			
+			// BOTON INICIO
+			// Imagen Boton Inicio
+			image(pantalla[1], xBotonInicio, yBotonInicio);
+			
+			// Si esta el mouse encima del Boton Inicio mostrar imagen Boton Inicio Oprimido
+			if (mouseX > xBotonInicio && mouseX < xBotonInicio + 106 
+				&& mouseY > yBotonInicio && mouseY < yBotonInicio + 107) {
+				image(pantalla[2], xBotonInicio, yBotonInicio);}
+			
+			// Cuando se le de clic en Boton Inicio pasar a la pantalla Instrucciones Generales
+			if (mouseX > xBotonInicio && mouseX < xBotonInicio + 106 
+				&& mouseY > yBotonInicio && mouseY < yBotonInicio + 107) {
+				estado = 1;}
+			
+			break;
+			
+		case 1:
+		//INSTRUCCIONES GENERALES
+			//Imagen Instrucciones Generales
+			imageMode(CENTER);
+			image(pantalla[3], 600, 350);
+			imageMode(CORNER);
+					
+			// BOTON INICIO
+			// Imagen Boton Instrucciones
+			image(pantalla[4], xBotonInstrucciones, yBotonInstrucciones);
+					
+			// Si esta el mouse encima del Boton Instrucciones mostrar imagen Boton Instrucciones Oprimido
+			if (mouseX > xBotonInstrucciones && mouseX < xBotonInstrucciones + 106 
+				&& mouseY > yBotonInstrucciones && mouseY < yBotonInstrucciones + 107) {
+				image(pantalla[5], xBotonInstrucciones, yBotonInstrucciones);}
+			
+			// Cuando se le de clic en Boton Inicio pasar a la pantalla Instrucciones Generales
+			if (mouseX > xBotonInstrucciones && mouseX < xBotonInstrucciones + 106 
+				&& mouseY > yBotonInstrucciones && mouseY < yBotonInstrucciones + 107) {
+				estado = 2;}
+					
 			break;
 		}
 	}
